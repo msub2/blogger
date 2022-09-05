a9c8c67cee43d5fdb78d28bbf5146a7bd4bab242
Daniel Adams <msub2official@gmail.com>
2022-09-05 14:52:43 -0400

    Uncomment search sass module

--- frontend/components/blog/Search/Search.jsx
+++ frontend/components/blog/Search/Search.jsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import { useState, useEffect } from 'react';
 import { blogSearch } from '../../../actions/blog';
-//import './Search.module.scss';
+import './Search.module.scss';
 
 const Search = () => {
   const [values, setValues] = useState({
